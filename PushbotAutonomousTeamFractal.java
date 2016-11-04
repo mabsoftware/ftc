1ef5b5ea0dc1979f07f63919dfcb82a57f1361e0
Max Bowman <maxabowman@gmail.com>
2016-11-03 20:41:47 -0500

    Added the framework for the autonomous program.

---
+++ PushbotAutonomousTeamFractal.java
@@ -0,0 +1,73 @@
+/* Team Fractals Autonomous Program */
+/* Last updated 11/03/16 */
+/* Uses same hardware init file as PushbotTeamFractal.java */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.Range;
+
+
+@Autonomous(name="Team Fractals Autonomous")
+//@Disabled
+public class PushbotAutonomousTeamFractal extends OpMode{
+
+    /* Declare OpMode members. */
+    Hardware robot;
+
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot = new Hardware(); // define the Pushbot's hardware.
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Ready.");    //
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+        telemetry.addData("Say", "Please press play.");
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        telemetry.addData("Say", "Starting...");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        // Code to run multiple times goes here.
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        telemetry.addData("Say", "Stopped.");
+    }
+
+}
