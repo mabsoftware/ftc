905ae4f10da5041d6fc19e110eda83fece35d7b2
Max Bowman <maxabowman@gmail.com>
2016-12-30 15:11:21 -0600

    *** Important Update *** SensorArray class added to keep track of sensors, wrote a test for red beacon strategy.

--- RedBeaconStrategy.java
+++ RedBeaconStrategy.java
@@ -1,4 +1,4 @@
-/* Team Fractals Autonomous Program if on Team Red, Station 1.
+/* Team Fractals Autonomous Program if on Team Red, beacon strategy.
  * Based on program by Robert Atkinson (2016)
  * Note: front is beacon pusher.
  * Negative power to the motors moves the robot
@@ -9,120 +9,38 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 
-@Autonomous(name="Red Team 1", group="Autonomous")
+@Autonomous(name="Red Team Beacon Strategy", group="Autonomous")
 public class RedBeaconStrategy extends LinearOpMode {
     Hardware robot = new Hardware(); // Initialize hardware.
     private ElapsedTime runtime = new ElapsedTime(); // Figure out how long the robot has been running.
-    private AutoDrive drive = new AutoDrive(this, robot);
-
-    // Declare sensor variables.
-    ColorSensor cSensor;
-    ColorSensing colorSensor;
-
-    // Constants for figuring distance using motor encoders.
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 5.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.14159265358979323846264338);
-    static final double     DRIVE_SPEED             = 0.6;
-    static final double     TURN_SPEED              = 0.5;
-
-    // Constants for turning the robot theta degrees.
-    static final double ROBOT_WIDTH = 16;
-    static final double WALL_OFFSET = 39;
+    private AutoDrive drive;
+    private SensorArray sensors;
 
     @Override
     public void runOpMode() {
-        robot.init(hardwareMap, true);
-
-        // Initialize sensors.
-
-        cSensor = hardwareMap.colorSensor.get("sensor_color");
-        colorSensor = new ColorSensing(cSensor);
-        colorSensor.setMode('p');
-        telemetry.addData("Status", "Sensors online.");
-        telemetry.update();
-
-        //*** Encoder Stuff ***//
-        telemetry.addData("Status", "Resetting encoders...");
-        telemetry.update();
-
-        robot.leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        idle();
-
-        robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.init(hardwareMap, true); // Initialize hardware with encoders.
+        drive = new AutoDrive(this, robot);
+        sensors = new SensorArray(this);
+        telemetry.addData("Message", "All systems online.");
 
         waitForStart(); // Wait until ready.
 
-        // *** Boiler Plate Code Done *** //
         // *** Main Code *** //
-        encoderDrive(DRIVE_SPEED,  6.0 * 12,  6.0 * 12, 5.0);
-        turn(90);
+        sleep(3000);
+        telemetry.addData("Color sensor test...", sensors.getBeaconColor()); // prints b for blue, r for red.
+        telemetry.update();
+        sleep(3000);
+        telemetry.addData("Drive test...", ":");
+        telemetry.update();
+        drive.forward(20);
+        drive.turn(90);
+        sleep(3000);
+        telemetry.addData("Touch sensor test...", sensors.isTouchingBeacon()); // prints true if being touched.
+        telemetry.update();
         // *** Main Code Done *** //
         telemetry.addData("Robot", "Done...");
         telemetry.update();
-    }
-
-    // Preconditions: theta is in the interval [-359, 359]
-    public void turn(double theta) {
-        robot.leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        // turn left if negative, right otherwise.
-        if (theta < 0) {
-            double distanceToMove = ROBOT_WIDTH * (-theta / 360) * Math.PI;
-            encoderDrive(DRIVE_SPEED, 0, distanceToMove, 5.0);
-        } else {
-            double distanceToMove = ROBOT_WIDTH * (theta / 360) * Math.PI;
-            encoderDrive(DRIVE_SPEED, distanceToMove, 0, 5.0);
-        }
-    }
-
-    public void encoderDrive(double speed, double leftInches, double rightInches, double timeoutS) {
-        int newLeftTarget;
-        int newRightTarget;
-
-        // Ensure that the opmode is still active
-        if (opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            newLeftTarget = robot.leftMotor.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
-            newRightTarget = robot.rightMotor.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
-            robot.leftMotor.setTargetPosition(newLeftTarget);
-            robot.rightMotor.setTargetPosition(newRightTarget);
-
-            // Turn On RUN_TO_POSITION
-            robot.leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            robot.leftMotor.setPower(Math.abs(speed));
-            robot.rightMotor.setPower(Math.abs(speed));
-
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (robot.leftMotor.isBusy() && robot.rightMotor.isBusy())) {
-
-                // Display it for the driver.
-                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
-                telemetry.addData("Path2",  "Running at %7d :%7d",
-                        robot.leftMotor.getCurrentPosition(),
-                        robot.rightMotor.getCurrentPosition());
-                telemetry.update();
-            }
-
-            // Stop all motion;
-            robot.leftMotor.setPower(0);
-            robot.rightMotor.setPower(0);
-
-            // Turn off RUN_TO_POSITION
-            robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
     }
 }
---
+++ SensorArray.java
@@ -0,0 +1,29 @@
+/* Sensor Array Class
+ * This class is meant to simplify getting sensor
+ * inputs. It makes it easy to initialize and
+ */
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+public class SensorArray {
+    private ColorSensor beaconcSensor;
+    private ColorSensing beaconColorSensor;
+    private TouchSensor beaconTouchSensor;
+    public SensorArray(LinearOpMode l) {
+        beaconcSensor = l.hardwareMap.colorSensor.get(Constants.beacon_color_sensor);
+        beaconColorSensor = new ColorSensing(beaconcSensor);
+        beaconTouchSensor = l.hardwareMap.touchSensor.get(Constants.beacon_touch_sensor);
+
+        beaconColorSensor.setMode('p');
+    }
+    public char getBeaconColor() {
+        return beaconColorSensor.getColor();
+    }
+
+    public boolean isTouchingBeacon() {
+        return beaconTouchSensor.isPressed();
+    }
+}
